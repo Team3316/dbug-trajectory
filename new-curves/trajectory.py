ce88111bca86722c9e7d442d26b41580d26fd617
Jonathan Ohayon <me@jonathano.com>
2018-09-01 15:26:12 +0300

    Added documentation to new trajectory

--- new-curves/trajectory.py
+++ new-curves/trajectory.py
@@ -31,6 +31,13 @@ class Trajectory:
 
     @classmethod
     def from_json(cls, trajectory_filename: str, robot_filename: str):
+        """
+        Initializes a new Trajectory object using data defined in a given JSON file.
+        :param trajectory_filename: The filename of the trajectory data file
+        :param robot_filename: The filename of the robot profile data file
+        :return: A new Trajectory instance, initialized with a list of Waypoints from the trajectory file and a Robot
+                 from the robot file.
+        """
         file = open(trajectory_filename, 'r').read()
         decoded = json.loads(file)
         waypoints = [
@@ -44,29 +51,38 @@ class Trajectory:
         robot = Robot.from_json(robot_filename)
         return cls(waypoints, robot)
 
-    def control_points(self, curve_type: CurveType = CurveType.POSITION):
+    def control_points(self):
+        """
+        Calculates the control points needed to calculate the curves.
+        :return: A list of numpy vectors holding all of the needed info for the curve.
+        """
         lw = len(self.waypoints)
         control_points = []
         for i in range(lw - 1):
             p0 = self.waypoints[i]
             p1 = self.waypoints[i + 1]
             dist = p0.distance_to(p1)
-            is_not_position = curve_type != CurveType.POSITION
 
             control_points.append(
                 nparray([
                     p0.point,
-                    p0.first_derivative(scale=dist * 1, velocity=is_not_position),
-                    p0.second_derivative(velocity=is_not_position),
+                    p0.first_derivative(scale=dist * 1),
+                    p0.second_derivative(),
                     p1.point,
-                    p1.first_derivative(scale=dist * 1, velocity=is_not_position),
-                    p1.second_derivative(velocity=is_not_position)
+                    p1.first_derivative(scale=dist * 1),
+                    p1.second_derivative()
                 ])
             )
 
         return control_points
 
     def curve(self, curve_type: CurveType, concat: bool = True):
+        """
+        Calculates the curve corresponding to the given type for the _middle_ of the robot.
+        :param curve_type: The curve type wanted to calculate
+        :param concat: Should concat the segments or not. Default - false
+        :return: A list of numpy point vectors if concat is false. Else - one huge numpy vector
+        """
         cp = self.control_points(curve_type)
 
         t = linspace(0, 1, samples=101)
@@ -78,6 +94,11 @@ class Trajectory:
         return npconcat(curves) if concat else curves
 
     def headings(self):
+        """
+        Calculates the robot's heading angles for each point in the curve (theta(t)) and calculates the derivative of
+        theta(t) (theta'(t)).
+        :return: A tuple consisting of the values of theta(t) and theta'(t) through the curve.
+        """
         cp = self.control_points()
 
         t = linspace(0, 1, samples=101)
@@ -89,6 +110,12 @@ class Trajectory:
         return angle_from_slope(dx, dy), ((d2y * dx - d2x * dy) / (dx ** 2 + dy ** 2))
 
     def robot_curve(self, curve_type: CurveType, side: RobotSide):
+        """
+        Calculates the given curve for the given side of the robot.
+        :param curve_type: The type of the curve to calculate
+        :param side: The side to use in the calculation
+        :return: The points of the calculated curve
+        """
         coeff = (self.robot.robot_info[3] / 2) * (1 if side == RobotSide.LEFT else -1)
         cp = self.control_points()
 
