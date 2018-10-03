10ceae49d9bdeaf84e916653db31753c41db91e8
Jonathan Ohayon <me@jonathano.com>
2018-10-03 11:30:57 +0300

    Added documentation to the utility functions

--- utils.py
+++ utils.py
@@ -10,6 +10,13 @@ NpCompatible = Union[float, List[float], np.float64, np.ndarray]
 
 
 def linspace(lb: float, ub: float, samples: int) -> np.ndarray:
+    """
+    Creates a Numpy linear space needed for spline / integral calculation.
+    :param lb: The lower bound of the linear space.
+    :param ub: The upper bound of the linear space.
+    :param samples: The number of samples to use in the linear space.
+    :return: The Numpy linear space
+    """
     sp = np.linspace(lb, ub, num=samples)
     m = np.linspace(0, 0, num=0)
     x, _ = np.meshgrid(sp, m, sparse=True)
@@ -17,6 +24,12 @@ def linspace(lb: float, ub: float, samples: int) -> np.ndarray:
 
 
 def angle_from_slope(dx: NpCompatible, dy: NpCompatible):
+    """
+    Returns the angle of the given slope dy/dx relative to the positive half of R^2.
+    :param dx: The change in the x direction.
+    :param dy: The change in the y direction.
+    :return: The angle **in degrees**
+    """
     if type(dx) != type(dy):
         raise ValueError('dx and dy should be the same type: either a float or a list of floats.')
     if type(dx) is np.float64 or type(dx) is float:
@@ -31,10 +44,10 @@ def length_integral(t0: float, t1: float, df: Callable[[float], Any], n: int):
     Calculates a segment's length using its derivative, from t0 to t1.
     The intergal is:
       I_t0^t1 sqrt((df_x(t))^2 + (df_y(t))^2)dt
-    :param t0: The lower bound of the integral, 0 <= t0 <= 1
-    :param t1: The upper bound of the integral, 0 <= t0 <= 1
+    :param t0: The lower bound of the integral, 0 <= t0 <= 1.
+    :param t1: The upper bound of the integral, 0 <= t0 <= 1.
     :param df: The function's derivative, as a function.
-    :param n: The number of samples in the given derivative array
+    :param n: The number of samples in the given derivative array.
     :return: An approximated segment length, in the segment's units, using Simpson's rule.
     """
     df_fix = lambda t: df(t)[0]  # This is needed because numpy is shit sometimes
@@ -54,15 +67,21 @@ def length_integral(t0: float, t1: float, df: Callable[[float], Any], n: int):
 def clamp_to_bounds(lb: float, ub: float, val: float):
     """
     Clamps the given value to the given bounds.
-    :param lb: The lower bound of the clamp
-    :param ub: The upper bound of the clamp
-    :param val: The value to clamp
+    :param lb: The lower bound of the clamp.
+    :param ub: The upper bound of the clamp.
+    :param val: The value to clamp.
     :return: The clamped value
     """
     return max(min(val, ub), lb)
 
 
 def vectorify(t: NpCompatible) -> np.ndarray:
+    """
+    Creates a Numpy vector out of a list, a number (int / float / float64) or a list.
+    ndarray -> ndarray, a: list -> np.array(a), x: Union[int, float, np.float64] -> np.array([[x]]).
+    :param t: The Numpy-compatible data to vectorify
+    :return: The numpy array
+    """
     if type(t) is float or type(t) is int or type(t) is np.float64:
         return np.array([[t]])
     elif type(t) is np.ndarray:
@@ -72,4 +91,14 @@ def vectorify(t: NpCompatible) -> np.ndarray:
 
 
 def sign(x: Union[float, int, np.float]) -> int:
+    """
+    Returns the sign of the input: -1, 0 or 1.
+    sign(x) = {
+        -1 if x < 0,
+        0 if x = 0
+        1 else
+    }
+    :param x: The number to return the sign of
+    :return: The sign of the input x
+    """
     return int(x) and (1, -1)[x < 0]
